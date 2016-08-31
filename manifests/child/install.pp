4fada1b9ca5f89b34184d60ddf55f1a809c05ea4
Lucas Emery <lemery@bottomline.com>
2016-08-31 17:05:22 -0400

    Updating child install package requirements

--- manifests/child/install.pp
+++ manifests/child/install.pp
@@ -1,4 +1,7 @@
 # Pulp Node Install Packages
 class pulp::child::install {
-  package { 'pulp-nodes-child': }
+  package { [
+    'pulp-agent',
+    'pulp-nodes-child',
+    'pulp-nodes-consumer-extensions']: }
 }
