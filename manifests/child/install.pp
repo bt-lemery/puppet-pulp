34ffc5df761ca42c2238989d5a622d022502286b
Lucas Emery <lemery@bottomline.com>
2016-09-02 10:19:07 -0400

    ensure_packages takes different syntax

--- manifests/child/install.pp
+++ manifests/child/install.pp
@@ -1,7 +1,7 @@
 # Pulp Node Install Packages
 class pulp::child::install {
-  ensure_packages({ [
+  ensure_packages([
     'pulp-agent',
     'pulp-nodes-child',
-    'pulp-nodes-consumer-extensions']: })
+    'pulp-nodes-consumer-extensions'])
 }
